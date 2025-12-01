742a7a7b1e7fdad526c6252b0d97f4ef5ed8009d
AndrewThelen1 <90250404+moldymelonade@users.noreply.github.com>
2025-12-01 17:11:45 -0500

    added day two solution, made each solution into distinct method

--- day_one/breaker.py
+++ day_one/breaker.py
@@ -1,16 +1,58 @@
-dial_ptr = 50
-file = open('Python/advent/day_one_input.txt', 'r')
-count = 0
 
-for line in file:
+# return the absolute distance the dial should turn
+def absolute_rotation(line):
+    #determines direction
     mult = 1
     if line[0] == 'L':
         mult = -1
+        
     distance = int(line[1:])
-    dial_ptr += (mult*distance)
-    dial_ptr %= 100
-    if dial_ptr == 0:
-        count+=1
+    return (mult*distance)
 
-file.close()
-print(count)
+
+def part_one():
+    count = 0
+    dial_ptr = 50
+    file = open('Python/advent/day_one/input.txt', 'r')
+    for line in file:
+        distance = absolute_rotation(line)
+        
+        #turn the dial the specified distance
+        dial_ptr += distance
+        
+        # round pointer to the actual position of the dial
+        dial_ptr %= 100
+        
+        #increment if on zero
+        if dial_ptr == 0:
+            count+=1
+    file.close()
+    return count
+
+
+def part_two():
+    count = 0
+    dial_ptr = 50
+    file = open('Python/advent/day_one/input.txt', 'r')
+    for line in file:
+        start = dial_ptr
+        distance = absolute_rotation(line)
+        
+        # turn the dial the specified distance 
+        dial_ptr += distance
+        
+        # left turns require an extra increment when zero is crossed
+        if dial_ptr <= 0 and start != 0:
+            count+=1
+
+        # determines how many times zero was crossed in turn
+        count+= abs(dial_ptr)//100
+        
+        # round pointer to the actual position of the dial
+        dial_ptr %= 100
+    file.close()
+    return count
+
+
+print(f'part one answer: {part_one()}')
+print(f'part two answer: {part_two()}')
