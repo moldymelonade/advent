c93ecce5ca2b54463a4a520a6704df6b1ec390cc
AndrewThelen1 <90250404+moldymelonade@users.noreply.github.com>
2025-12-03 13:30:58 -0500

    added comments.

--- day_3/part_one.py
+++ day_3/part_one.py
@@ -3,13 +3,18 @@ content = file.readlines()
 file.close()
 
 
+#get the largest number between indices start and end
 def get_largest(start, end):
     largestIndex = start
+    
     for i in range(start,end):
         curr = int(line[i])
+        
+        #9 is the largest possible number
         if curr == 9:
             largestIndex = i
             break
+        
         if curr > int(line[largestIndex]):
             largestIndex = i
     return largestIndex
@@ -19,8 +24,15 @@ total = 0
 for line in content:
     line = line.strip()
     length = len(line)
+    
+    # get largest number.
+    # make sure to leave enough space to form a two digit number.
     largest = get_largest(0,length-1)
     second = get_largest(largest+1, length)
+    
+    #convert number string to int
     num = int(line[largest] + line[second])
+    
     total += num
+    
 print(total)
--- day_3/part_two.py
+++ day_3/part_two.py
@@ -3,13 +3,18 @@ content = file.readlines()
 file.close()
 
 
+#get the largest number between indices start and end
 def get_largest(start, end):
     largestIndex = start
+    
     for i in range(start,end):
         curr = int(line[i])
+        
+        #9 is the largest possible number
         if curr == 9:
             largestIndex = i
             break
+        
         if curr > int(line[largestIndex]):
             largestIndex = i
     return largestIndex
@@ -20,16 +25,27 @@ for line in content:
     digit_indices = []
     line = line.strip()
     length = len(line)
+    
     for digits_left in range(12,0,-1):
+        
+        # if this is the first iteration, start at 0.
+        # if not, start at the latest index + 1
         start = 0
         if len(digit_indices) > 0:
             start = digit_indices[-1]+1
+        
+        # leave enough room for the number to have 12 digits 
         end = length-digits_left+1
+        
         digit_indices.append(get_largest(start,end))
+    
+    #create a string from all of the logged indices
     numstring = ''
     for i in digit_indices:
         numstring += line[i]
+        
+    #convert the string to an int and add to total
     num = int(numstring)
-    print(num)
     total += num
+
 print(total)
